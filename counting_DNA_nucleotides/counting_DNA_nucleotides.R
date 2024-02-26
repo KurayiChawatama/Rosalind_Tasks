5ad73b19f70a939dbc58380b8cff2a9b612d7991
Kurayi Chawatama <142725139+KurayiChawatama@users.noreply.github.com>
2024-02-26 11:35:57 +0300

    Updated counting_DNA_nucleotides.R

--- counting_DNA_nucleotides/counting_DNA_nucleotides.R
+++ counting_DNA_nucleotides/counting_DNA_nucleotides.R
@@ -1,4 +1,4 @@
-sequence <- readLines("~/RProjects/Rosalind_Tasks/counting_DNA_nucleotides/rosalind_dna.txt")
+sequence <- readLines("rosalind_dna.txt")
 sequence <- unlist(strsplit(sequence, ""))
 
 for (i in c("A", "C", "G", "T")) {
