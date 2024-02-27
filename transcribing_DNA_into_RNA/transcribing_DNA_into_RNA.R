ddc99e65fe3f7f6c31e5a76b18066ae1f423e88f
KurayiChawatama <kurichawaz@gmail.com>
2024-02-27 09:21:55 +0300

    Completed the R version of transcribing_DNA_into_RNA task

--- transcribing_DNA_into_RNA/transcribing_DNA_into_RNA.R
+++ transcribing_DNA_into_RNA/transcribing_DNA_into_RNA.R
@@ -1,4 +1,4 @@
-sequence <- readLines("~/RProjects/Rosalind_Tasks/transcribing_DNA_into_RNA/rosalind_rna.txt")
+sequence <- readLines("rosalind_rna.txt")
 sequence <- unlist(strsplit(sequence, ""))
 
 RNA_sequence <- (gsub("T", "U", sequence))
